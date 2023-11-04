d172d38fa893052940ec69e7788fae698f4ba01e
Michael Vega <micha3lvega@gmail.com>
2023-11-03 23:29:46 -0500

    Manejo de excepciones

---
+++ src/main/java/co/com/michael/blacklist/services/exception/response/ExceptionResponse.java
@@ -0,0 +1,26 @@
+package co.com.michael.blacklist.services.exception.response;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse implements Serializable {
+
+	private static final long serialVersionUID = 1072844002741472252L;
+
+	private Date date;
+	private String message;
+	private String requestURL;
+	private HttpStatus responseCode;
+
+}
