8e61f92a1cd4cf4c5c687f74a068954936808d14
Michael Vega <micha3lvega@gmail.com>
2023-11-03 23:06:54 -0500

    Inicializar proyecto

---
+++ src/test/java/co/com/michael/blacklist/services/BlacklistServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.michael.blacklist.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlacklistServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
